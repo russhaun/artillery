58ea02f5da04f56b6cd9218be845880bd62ec45b
Russ Haun <artillerytesting@gmail.com>
2025-09-07 18:05:15 -0400

    New restart_server.py
    This file replaces the old  restart_server file. On Windows now has support for start/stop/restart ability. simple cmd line parser using argparse

--- restart_server.py
+++ restart_server.py
@@ -1,149 +1,212 @@
+##restart_server.py responsible for handling stop/start/restart abilities for artillery
+from src.core import signal,argparse,time,sys,os,errno,threading,subprocess,settings, log_event, is_posix, is_windows
 
-import subprocess
-import os
-from subprocess import CalledProcessError
-import time
-import sys
-from pathlib import PureWindowsPath
-from src.core import *
-##THIS ENTIRE SCRIPT WILL BE REPLACED IN FUTURE VERSIONS
-##WITH A MORE ROBUST CMDLINE UTILITY IN PROGRESS WHICH WILL CONSOLIDATE
-##ALL ARTILLERY CMDLINE TOOLS INTO ONE UTILITY DEALING WITH SERVICE MANAGEMENT
-##THIS INCLUDES THE ABILITY TO START, STOP, RESTART AND CHECK SERVICE STATUS 
-#THIS FILE WILL BE UPDATED TO USE THE SAME METHODS AS SERVICEMANAGER FROM TRAY APP
-
-
-#set some stuff up just for windows
-if 'win32' in sys.platform:
+if is_windows():
+    import wmi
+    #win32gui is used in the raw python file version
     import win32gui
     import win32process
     from win32api import GetUserNameEx
-    from src.pyuac import isUserAdmin, runAsAdmin
-    
-    EXE_PATH = settings.get_config('global',"APP_PATH")
-    EXE_FILE = settings.get_config('global',"APP_FILE")
-    PID_INFO_PATH = settings.get_config('global',"PIDFILE")
-    PID = []
-    #userneme in domain\user format
+    from src.core import isUserAdmin, runAsAdmin
+
     U_INFO = GetUserNameEx(2)
 
+class SVCMgr():
+    def __init__(self):
+        if is_windows():
+            self.pid = win32process.GetCurrentProcessId()
+            log_event(f"[*] Artillery Service controller starting with pid: {str(self.pid)}", 0, None, True)
+        if is_posix():
+            self.pid = os.getpid()
+            log_event(f"[*] Artillery Service controller starting with pid: {str(self.pid)}", 0, None, True)
+            
+    def is_service_active(self)->tuple[bool,str,str]:
+        '''
+        checks to see if process is running already using wmi.Returns a tuple of bool,pid,msg
+        '''
+        if is_windows():
+            try: #some times this is wrong and returns incorrect pid causing fuction to fail
+                #it finds pythons pid instead. i use this to find window
+                if win32gui.FindWindow('ConsoleWindowClass', 'Artillery - Advanced Threat Detection'):
+                    hwnd = win32gui.FindWindow('ConsoleWindowClass', 'Artillery - Advanced Threat Detection')
+                    threadid, pid = win32process.GetWindowThreadProcessId(hwnd)
+                    #i compare the values from this function and pid.txt if diiferent use pid.txt
+                    txt_id = []
+                    with open(file=settings.get_config('global',"PIDFILE"),mode='r',encoding='utf-8') as pidtxt:
+                        for line in pidtxt:
+                            txt_id.append(line)
+                    if pid == int(txt_id[0]):
+                        #use pid
+                        pass
+                    else:
+                        #use pid.txt
+                        pid = str(txt_id[0])
+                    #return a string to use with taskkill
+                    return (True,str(pid),f"[*] Service is running with pid of: {str(pid)}")
+                else:
+                    return (False,"0","[*] Service is not running.")
+            except win32gui.error as err:
+                pass
+                
+        if is_posix():
+            try:
+                proc = subprocess.Popen(
+                    "ps -A x | grep artillery", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+                pid, err = proc.communicate()
+                pid = [int(x.strip()) for line in pid.split()
+                       for x in line.split() if int(x.isdigit())]
+                if len(pid) > 0:
+                    return (True, str(pid[0]), "[*] Service is running")
+                else:
+                    return (False, "0", "[*] Service is not running")
+            except Exception as e:
+                log_event(f"[*] Error occurred while checking status: {str(e)}", 2, None, True)
+                return (False, "0", "[*] Error occurred while checking status")
 
-def GrabBootLoader():
-    '''looks for artillery window process and returns id. it checks
-     for console version 1st and then ui version. Returns None if not found.'''
-    try:
-        if win32gui.FindWindow('ConsoleWindowClass', 'Artillery - Advanced Threat Detection'):
-            hwnd = win32gui.FindWindow('ConsoleWindowClass', 'Artillery - Advanced Threat Detection')
-            threadid, pid = win32process.GetWindowThreadProcessId(hwnd)
-            #return a string to use with taskkill
-            return str(pid)
-        else:
-            if win32gui.FindWindow(None, 'Artillery Shell'):
-                hwnd = win32gui.FindWindow(None, 'Artillery Shell')
-                threadid, pid = win32process.GetWindowThreadProcessId(hwnd)
-                #return a string to use with taskkill
-                return str(pid)
-    except win32gui.error as err:
-        return False
-
-
-def kill_artillery_win():
-    '''opens pid.txt file made from artillery.exe or artilleryui.exe to grab current
-    PID to terminate. Note pyinstaller exe's create 2 running instances.
-    1 for bootloader and 1 for actual code. for this to work we need to kill both'''
-    try:
-        if os.path.isfile(PID_INFO_PATH):
-            log_event("[*] Finding Process info.....", 0, None, True)
-            #grab bootloader id
-            bootloader = GrabBootLoader()
-            #read main id from file
-            with open(PID_INFO_PATH, 'r') as p_id:
-                for line in p_id:
-                    line = line.strip()
-                    PID.append(line)
-            p_id.close()
-            mainwindow = PID[0]
-            if bootloader:
-                #this could all be condensed into one call but for clarity
-                log_event("[*] Bootloader ProcessID: " + bootloader, 0, None, True)
-                log_event("[*] MainWindow ProcessID: " + mainwindow, 0, None, True)
-                log_event('[*] Attempting to kill Artillery now.....', 0, None, True)
-                log_event("[!] killing python with a big sword.....", 0, None, True)
+    def start(self):
+        """
+        Starts artillery binary
+        """
+        if is_windows():
+            log_event("[*] Trying to start Artillery.", 0, None, True)
+            #check to see if we are already running if so
+            #exit and do not try to start a new instance
+            is_runninig = self.status()
+            service_running = is_runninig[0]
+            service_msg = is_runninig[1]
+            if service_running is True:
+                #if we are already running then just exit
+                log_event(service_msg, 0, None, True)
+                sys.exit()
+            else:
+                #if not running then start it
+                INSTALL_PATH = settings.get_config('global','APP_PATH')
                 try:
-                    #kill boot loader that was found.
-                    kill_bootloader = subprocess.check_call(['cmd', '/C', 'taskkill', '/PID', bootloader], shell=True)
-                    log_event("[!] Sucessfully removed it's head.....", 0, None, True)
-                    #ArtilleryStopEvent()
-                    return True
-                except CalledProcessError as err:
-                    log_event("[*] Looks like this process is dead already.", 0, None, True)
-                    return False
+                    executable = os.path.join(INSTALL_PATH, "Artillery.py")
+                    subprocess.call(['cmd', '/C', 'python', executable],shell=True,creationflags=subprocess.DETACHED_PROCESS,timeout=3)
+                except subprocess.TimeoutExpired as a:
+                    #pass on time out we are aware
+                    pass
+                finally:
+                    #5 secs is more then enough 
+                    #time to wait to verify it started
+                    service_check = self.status()
+                    service_running = service_check[0]
+                    service_pid = service_check[1]
+                    service_msg = service_check[2]
+                    if service_running is True:
+                        log_event(service_msg, 0, None, True)
+                    else:
+                        log_event(service_msg, 1, None, True)
+        if is_posix():
+            log_event("[*] Attempting to find Artillery service", 0, None, True)
+            if os.path.isfile("/var/artillery/artillery.py"):
+                log_event("[*] Starting Artillery server process...", 0, None, True)
+                subprocess.Popen(["python3", "/var/artillery/artillery.py", "&"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, start_new_session=True)
             else:
-                log_event("[!] Bootloader process not present.....", 0, None, True)
-                return False
-        else:
-            log_event('[*] pid.txt was not found\n[*] Artillery must be run @ least once.......', 1, None, True)
-            pause = input("[*] File was not found press enter to quit:")
-    except FileNotFoundError as err:
-        pass
+                log_event("[!] Artillery server script not found at /var/artillery/artillery.py", 1, None, True)
 
-
-def restart_artillery_win():
-    '''restarts main exe by calling after waiting a few seconds
-    to allow previous instance if any to close down'''
-    # check to see if artillery is running
-    check = kill_artillery_win()
-    if check:
-        log_event("[!] Process Killed\n[*] Launching now..... ", 0, None, True)
-        subprocess.call(['cmd', '/C', 'cls'])
-        #make sure proccess is dead wait a sec
-        time.sleep(1)
-        try:
-            if os.path.isdir(EXE_PATH):
-                #opens exe in seperate window
-                os.system(f"start cmd /K artillery_start.bat")
-                return
+               
+    def stop(self):
+        '''
+        stops main exe by using os.kill()
+        '''
+        if is_windows():
+            #log_event("[*] Attempting to stop service", 0, None, True)
+            service_status = self.status()
+            service_running = service_status[0]
+            service_pid = str(service_status[1])
+            service_msg = str(service_status[2])
+            #check to see if service is running if false just quit
+            if service_running is False:
+                log_event(service_msg, 0, None, True)
+                sys.exit()
             else:
-                pause = input('[*] artillery_start.bat was not found. Please make sure the file exists.\n[*] Press enter to continue')
-        except FileNotFoundError as e:
-            pass
-    else:
-        log_event("[*] Launching now..... ", 0, None, True)
-        time.sleep(3)
-        try:
-            if os.path.isdir(EXE_PATH):
-                #opens exe in seperate window
-                os.system("start cmd /K artillery_start.bat")
+                log_event(service_msg, 0, None, True)
+                try:
+                    msg = f"[*] Killing Artillery with processID of: {service_pid}"
+                    #logs.info(msg)
+                    log_event(msg, 0, None, True)
+                    try:
+                        #works no errors but still not clean find a better way?
+                        os.kill(int(service_pid),signal.SIGTERM)
+                        log_event("[*] Artillery process killed successfully.", 0, None, True)
+                        return
+                    except SystemError as e:
+                        log_event(f"[*] SystemError occurred: {str(e)}", 2, None, True)
+                        return
+                except WindowsError as werr:
+                    log_event(f"[*] WindowsError occurred: {str(werr)}", 2, None, True)
+                    return
+        if is_posix():
+            #this is the kill_artillery func from core.py
+            log_event("[*] Attempting to stop Artillery service", 0, None, True)
+            try:
+                proc = subprocess.Popen(
+                    "ps -A x | grep artiller[y]", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+                pid, err = proc.communicate()
+                pid = [int(x.strip()) for line in pid.split()
+                    for x in line.split() if int(x.isdigit())]
+                log_event("Killing the old Artillery process...",0,None,True)
+                for i in pid:
+                    os.kill(i, signal.SIGKILL)
+                log_event("[*] Artillery process killed successfully.", 0, None, True)
+            except Exception as e:
+                log_event(f"[*] Error occurred while trying to stop Artillery: {str(e)}", 2, None, True)
+    
+    
+    def status(self)->tuple[bool,str,str]:
+        '''
+        Checks to see if service is running. Returns a tuple of bool,msg,pid
+
+        '''
+        is_active = self.is_service_active()
+        active = is_active[0]
+        #
+        if active == True:
+            return (True, str(is_active[1]), str(is_active[2]))
+        elif active == False or None:
+            return (False, str(is_active[1]), str(is_active[2]))
+       
+
+    def restart(self):
+        """
+        Restarts the artillery service.
+        """
+        if is_windows():
+            log_event("[*] Attempting to restart Artillery service", 0, None, True)
+            status = self.status()
+            if status[0] is True:
+                self.stop()
+                time.sleep(5)
+                #sleep for a sec to allow process to close
+                #check to see if it is still running
+                status = self.status()
+                if status[0] is False:
+                    #then start it again
+                    #log_event(f"{status[1]}, starting it now.", 0, None, True)
+                    self.start()
             else:
-                pause = input('[*] artillery_start.bat was not found. Please make sure the file exists.\n[*] Press enter to continue')
-        except FileNotFoundError as e:
-            pass
+                #log_event(f"{status[1]}, starting it now.", 0, None, True)
+                self.start()
+                #sleep for a sec to allow process to start
+                time.sleep(5)
+                #check to see if it started
+                status = self.status()
+                if status[0] is True:
+                    log_event(f"{status[2]}", 0, None, True)
+                else:
+                    log_event(f"{status[2]}", 1, None, True)
+            
+        if is_posix():
+            log_event("[*] Attempting to restart Artillery service", 0, None, True)
+            self.stop()
+            time.sleep(5)  # wait for the process to close
+            self.start()
+    
+    
+    
 
-
-def main():
-    cmd = ""
-    try:
-        cmd = input("[*] Restart Artillery instance?:\n[*] Kill Artillery instance?:\n[*] Type restart or kill respectivley\n[*] Type exit to quit\n[*] \\:")
-    except Exception as e:
-        print(str(e))
-    result = cmd
-    if result == 'kill':
-        kill_artillery_win()
-        looper()
-    elif result == 'restart':
-        restart_artillery_win()
-        looper()
-    elif result == 'exit':
-        log_event("Closing software please wait.....", 0, None, True)
-        time.sleep(3)
-        sys.exit()
-    else:
-        log_event(f"[!] Unknown command: {cmd}", 1, None, True)
-        looper()
-
-
-def looper():
-    main()
 
 
 if __name__ == "__main__":
@@ -152,13 +215,45 @@ if __name__ == "__main__":
             runAsAdmin()
             sys.exit(1)
         if isUserAdmin():
-            time.sleep(2)
             log_event(f"[*] Running as: {U_INFO}", 0, None, True)
-            looper()
-    if ('linux' or 'linux2' or 'darwin') in sys.platform:
-        # kill running instance of artillery
-        kill_artillery()
-        #
-        if os.path.isfile("/var/artillery/artillery.py"):
-            log_event("Restarting the Artillery Server process...",0,None,True)
-            subprocess.Popen(["python3", "/var/artillery/artillery.py", "&"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, start_new_session=True)
+    
+    if is_posix():
+        #import errno
+        try:   # and delete folder
+            if os.path.isdir("/var/artillery_check_root"):
+                os.rmdir('/var/artillery_check_root')
+                #if not thow error and quit
+        except OSError as e:
+            if (e.errno == errno.EACCES or e.errno == errno.EPERM):
+                log_event("You must be root to run this script!\r\n",0,None,True)
+                sys.exit(1)
+        #if we got this far re-create root_check folder
+        if not os.path.isdir("/var/artillery_check_root"):
+            os.mkdir("/var/artillery_check_root")
+            #set permisions to 700
+            os.chmod("/var/artillery_check_root", 0o700)
+    
+    #setup our parser to accept cmd line options.
+    #for now it is basic more options wil be added over time.
+    service = SVCMgr()
+    parser = argparse.ArgumentParser(prog='restart',usage='%(prog)s [options]',description='Restart manager for Artillery',add_help=True)
+    subparser = parser.add_subparsers()
+    start_parser = subparser.add_parser(name='start', help='starts artillery process.')
+    start_parser.add_argument('start', action='store_true')
+    start_parser.set_defaults(function=service.start)
+    stop_parser = subparser.add_parser(name='stop', help='stops artillery process.')
+    stop_parser.add_argument("stop", action='store_true')
+    stop_parser.set_defaults(function=service.stop)
+    restart_parser = subparser.add_parser(name='restart', help='restarts artillery process.')
+    restart_parser.add_argument("restart", action='store_true')
+    restart_parser.set_defaults(function=service.restart)
+    args = parser.parse_args()
+    try:
+        if hasattr(args, 'function'):
+            args.function()
+    except Exception as e:
+        #if error print help
+        #print(e)
+        log_event("[!] No valid command provided. Use 'start', 'stop', or 'restart'.", 1, None, True)
+        parser.print_help()
+   
